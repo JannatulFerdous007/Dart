f726de7f34ff1ad3156b7cc8c151340f2f35944b
JannatulFerdous007 <jannatulferdous3853@gmail.com>
2026-05-14 15:45:32 +0600

    Initial commit

---
+++ bin/dart_fundamental.dart
@@ -0,0 +1,68 @@
+void main(){
+
+  // 1. Variable, var, final and const
+  // print("Hello World");
+  // var studentName = "Motiur";
+  // studentName = "Motiur Rahman";
+  // var age = 25;
+  // var ismale = true;
+  // var height = 5.9;
+  // print("$studentName is $age years old.");
+
+
+  // //final(run time error)
+  // final String country = "Dhaka";
+  // //country = "England"; //will show error
+  // final today = DateTime.now();
+  // print(today);
+  // print(country);
+
+  // //constant(compile time error)
+  // const String city = "Dhaka"; //This will cause an error because city is a constant
+
+
+  // 2. Data type
+  // int age = 10;
+  // double height = 1.75;
+  // String name = "Morshed";
+  // String description = """
+  // morshed is a software engineer with expertise in Dart Programming.
+  // """;
+  // bool isStudent = true;
+  // //row String
+  // //var path = r"C:\Users\Default\\morshed\Desktop\dart-fundamental.dart";'';
+  // print ("Name: $name, Age: $age, Height: $height, Is Student: $isStudent, His descrition is: $description");
+
+  //if-else
+  // var temperature = 30;
+  // if (temperature > 25){
+  //   print("It's a hot day");
+  // } else {
+  //   print("It's a pleasant day");
+  // }
+
+  //switch
+  // var day = "Friday";
+  // switch(day){
+  //   case "Monday":
+  //   print("It's a monday");
+  //   break;
+  //   case "Sunday":
+  //   print("It's a sunday");
+  //   break;
+  //   case "Tuesday":
+  //   print("It's a Tuesday");
+  //   break;
+  //   case "Wednesday":
+  //   print("It's a wednesday");
+  //   break;
+  //   case "Thurssday":
+  //   print("It's a thursday");
+  //   default:
+  //   print("Its a weekend");
+  // }
+
+  // String? nickname = null;
+  // //nullable(??)
+  // print("Nickname: ${nickname ?? "Taj"}");
+}
---
+++ bin/studentcligrading.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main() {
+  const String instituteName = "Ostad";
+  final DateTime now = DateTime.now();
+  print("""========================
+  Institute Name: $instituteName
+  Date: ${now}
+  ====================
+  """);
+  
+  var grade;
+  int marks;
+  String? name;
+  stdout.write("Enter your name: ");
+  name = name?.trim() ?? "Name not provided";
+  name=stdin.readLineSync()!;
+  print(name);
+  stdout.write("Enter your marks: ");
+  marks = int.parse(stdin.readLineSync()!);
+  print("Your marks: $marks");
+
+  if (marks >= 80){
+    grade = "A+";
+  } else if (marks >= 60){
+    grade = "A";
+  } else if (marks >= 60){
+    grade = "A-";
+  }else if (marks >= 60){
+    grade = "B";
+  } else {
+    grade= "F";
+  } 
+  print("Your grade: $grade");
+
+  stdout.write("Enter your marks: ");
+  marks = int.parse(stdin.readLineSync()!);
+  print("Your marks: $marks");
+}
